2cae6f2936818b89241303d3fb9c7667a07c96f2
James Vickery <jamesevickery@users.noreply.github.com>
2017-04-04 01:37:17 +0100

    Comments and a few improvements

--- CrimeSolver/Program.cs
+++ CrimeSolver/Program.cs
@@ -5,16 +5,26 @@ namespace CrimeSolver
 {
     class Program
     {
+        /// <summary>
+        /// This is the main function of the code, where the code starts.
+        /// It calls the SolveCrime function then uses a ReadLine to hold the terminal.
+        /// </summary>
         static void Main(string[] args)
         {
             SolveCrime();
             Console.ReadLine();
         }
 
-        static void SolveCrime()
+        /// <summary>
+        /// The method to test every possible combination of true and false.
+        /// </summary>
+        private static void SolveCrime()
         {
+            // Create a new array of boolean values: `true` and `false`.
+            // This just saves us from typing {true, false} for every foreach statement
             bool[] bools = new bool[] { true, false };
 
+            // The foreach statements run for every value of `bools` (true and false)
             foreach (bool a in bools)
             {
                 foreach (bool b in bools)
@@ -23,40 +33,53 @@ namespace CrimeSolver
                     {
                         foreach (bool d in bools)
                         {
+                            // The `Valid` function is called with the suspects' statements.
+                            // If all return `true` (mathematical tautology) then whoWasIt is called
                             if (Valid(!d, !b) &
                                 Valid(!c, d) &
                                 Valid(a, !d) &
                                 Valid(c, !a))
                             {
-                                bool a_static = a;
-                                bool b_static = b;
-                                bool c_static = c;
-                                bool d_static = d;
-                                Console.WriteLine(whoWasIt(ref a_static, ref b_static, ref c_static, ref d_static));
+                                bool[] s = new bool[] { a, b, c, d };
+                                // Print list of convicts from whoWasIt method
+                                Console.WriteLine(whoWasIt(ref s[0], ref s[1], ref s[2], ref s[3]));
                             }
                         }
                     }
                 }
             }
-
         }
 
-        static bool Valid(bool x, bool y)
+        /// <summary>
+        /// Returns `true` if x or y is true (but not both)
+        /// So returns `false` if neither, or both, are true.
+        /// </summary>
+        private static bool Valid(bool x, bool y)
         {
             return ((!x && y) || (x && !y));
         }
 
-        static string whoWasIt<T>(ref T a, ref T b, ref T c, ref T d)
+        /// <summary>
+        /// Outputs the name(s) of the convicted suspects
+        /// </summary>
+        /// <returns>A string, listing the convicts</returns>
+        private static string whoWasIt<T>(ref T a, ref T b, ref T c, ref T d)
         {
+            // Create list for convict names
+            // IList Requires .NET 4+
             IList<string> names = new List<string>();
-            // Requires .NET 4+
+
+            // If the boolean value of the string of the suspects' variables are true, add them to a list of names
             if (bool.Parse(a.ToString())) names.Add("Alex");
             if (bool.Parse(b.ToString())) names.Add("Brad");
             if (bool.Parse(c.ToString())) names.Add("Charlie");
             if (bool.Parse(d.ToString())) names.Add("David");
+
+            // Create readable string of convict names
             string convicts = string.Join(", ", names);
 
-            string result = "The crime was commited by: ";
+            // Generate resultant string and return to SolveCrime
+            string result = "Possible convict(s): The crime was commited by: ";
             result += convicts + ".";
             return result;
         }
